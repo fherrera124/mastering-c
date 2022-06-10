877b8c03f2674628016a0ece88251631562de586
Francisco Herrera <fran.herrera124@gmail.com>
2022-06-10 05:15:27 -0300

    update settings

--- K-R-solutions/ch03/ex3_6.c
+++ K-R-solutions/ch03/ex3_6.c
@@ -3,26 +3,21 @@
 
 #define MAX 1000
 
-int main(void) {
+void itoa(int n, char s[], int w);
 
+int main(void) {
     char s[MAX];
 
-    itoa2(100,s,3);
+    itoa(100, s, 3);
 
     return 0;
 }
-
 
 /*
 Exercise 3-6. Write a version of itoa that accepts three arguments instead of two. The third
 argument is a minimum field width; the converted number must be padded with blanks on the
 left if necessary to make it wide enough.
 */
-void itoa2(int n, char s[], int w) {
-
-    itoa(n, s);
-
+void itoa(int n, char s[], int w) {
     printf("%d\n", strlen(s));
-
-
 }
